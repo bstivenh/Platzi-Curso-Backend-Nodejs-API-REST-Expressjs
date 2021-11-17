fc0b47cdecd7b178650d73eb856cecc8062ad453
Stiven Hernandez <stiven.hernandez@nexusw.co>
2021-11-17 15:26:26 -0500

    Clase 15: Async await y captura de errores

--- services/product.service.js
+++ services/product.service.js
@@ -6,7 +6,7 @@ class ProductsService {
     this.generate();
   }
 
-  generate() {
+  async generate() {
     const limit = 100;
 
     for (let index = 0; index < limit; index++) {
@@ -19,7 +19,7 @@ class ProductsService {
     }
   }
 
-  create(data) {
+  async create(data) {
     const { name, price, image } = data;
     const product = {
         id: faker.datatype.uuid(),
@@ -31,15 +31,20 @@ class ProductsService {
     return product;
   }
 
-  find() {
-    return this.products;
+  async find() {
+    // return this.products;
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.products);
+      }, 5000);
+    });
   }
 
-  findOne(id) {
+  async findOne(id) {
     return this.products.find(item => item.id === id)
   }
 
-  update(id, changes) {
+  async update(id, changes) {
     const index = this.products.findIndex(item => item.id === id);
     if (index === -1) {
       throw new Error('Product not found');
@@ -52,7 +57,7 @@ class ProductsService {
     return this.products[index];
   }
 
-  delete(id) {
+  async delete(id) {
     const index = this.products.findIndex(item => item.id === id);
     if (index === -1) {
       throw new Error('Product not found');
